33a263a35460096d0090e04aadefeca0c98d38c9
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-07-07 01:47:57 +0530

    Created and Modified my own amazing searching algorithm.

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,45 +1,73 @@
 'use client'
 import Promptcard from "@/components/Promptcard"
-import {  db } from "@/firebase/firebaseConfig"
+import { db } from "@/firebase/firebaseConfig"
 import { PostData } from "@/types/typescript.types"
-import { Query, DocumentData, query, collection, orderBy, limit, QuerySnapshot, getDocs, where } from "firebase/firestore"
+import { Query, DocumentData, query, collection, orderBy, QuerySnapshot, getDocs } from "firebase/firestore"
 import { useSearchParams } from "next/navigation"
 import { useEffect, useState } from "react"
+import Fuse from 'fuse.js';
 
 const page = () => {
     const searchParams = useSearchParams()
     const search = searchParams.get('search_query')
-    console.log(search)
     const cleanedSearchQuery = search?.toLowerCase().trim().replace(/[^\w\s]+/g, '').replace(/\s+/g, '');
-    const lowercaseSearchQueryEnd = cleanedSearchQuery + '\uf8ff';
 
     const [myPrompts, setMyPrompts] = useState<PostData[]>([])
     const [loading, setLoading] = useState(false)
+
     const getMyPrompts = async () => {
         setLoading(true);
+
         const snippetQuery: Query<DocumentData> = query(
-            collection(db, "posts"),
-            orderBy("tag"),
-            orderBy("createdAt", "desc"),
-            where("tag", ">=", cleanedSearchQuery),
-            where("tag", "<=", lowercaseSearchQueryEnd)
+            collection(db, 'posts'),
+            orderBy('tag'),
+            orderBy('createdAt', 'desc')
         );
+
         const snippetDocs: QuerySnapshot<DocumentData> = await getDocs(snippetQuery);
-        const posts: PostData[] = snippetDocs.docs.map((doc) => {
-            const data = doc.data() as PostData; // Explicitly cast doc.data() as PostData
-            return {
-                ...data,
-            };
+
+        // Perform fuzzy matching on the search query against the database results
+        const matchedResults = cleanedSearchQuery ? fuzzyMatch(cleanedSearchQuery, snippetDocs.docs.map((doc) => doc.data())) : [];
+        const posts: PostData[] = matchedResults.map((result) => {
+            const matchingDoc = snippetDocs.docs.find((doc) => (doc.data().tag === result.tag) || doc.data().prompt === result.prompt || doc.data().creatorName
+                === result.creatorName
+            );
+            if (matchingDoc) {
+                const data = matchingDoc.data() as PostData;
+                return {
+                    ...data,
+                };
+            }
+            return null;
+        }).filter(Boolean) as PostData[];
+
+        setMyPrompts(posts);
+        setLoading(false);
+    };
+
+    // Function to perform fuzzy matching
+    function fuzzyMatch(query: string, results: DocumentData[]) {
+        const fuse = new Fuse(results, {
+            keys: ['tag', 'prompt', 'creatorName'], // Use 'tag' property for matching
+            includeScore: true,
+            threshold: 0.2, // Adjust the threshold value to control the matching sensitivity
         });
-        setMyPrompts(posts)
-        setLoading(false)
+
+        const matches = fuse.search(query);
+        const sortedMatches = matches.sort((a, b) => a.score! - b.score!); // Sort based on the score
+        const matchedResults = sortedMatches.map((match) => match.item); // Extract the matched document
+
+        return matchedResults;
     }
+
     useEffect(() => {
         getMyPrompts()
     }, [search])
+
     if (loading) {
         return <p className="text-red-600 text-center text-4xl">Loading...</p>
     }
+
     return <>
         <div className="mx-auto max-w-6xl px-6 lg:px-8">
             <h1 className='pt-14 text-5xl font-bold leading-[1.15] text-black sm:text-6xl text-center'>
