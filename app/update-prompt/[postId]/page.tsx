00f9cc14465f38f404ed47c81225f1631a3f29d7
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-07-08 23:02:18 +0530

    Refactor update prompt page code.

--- app/update-prompt/[postId]/page.tsx
+++ app/update-prompt/[postId]/page.tsx
@@ -3,10 +3,11 @@ import { db } from "@/firebase/firebaseConfig";
 import Form from "@/components/Form";
 
 const page = async ({ params }: { params: { postId: string } }) => {
-  const { postId } = params;
-  const docSnapshot = await getDoc(doc(db, "posts", postId));
-  const getPromptsById = docSnapshot.data();
-  
+  const { postId } = params; // Extracting postId from params object
+  const docSnapshot = await getDoc(doc(db, "posts", postId)); // Retrieving document snapshot for the specified postId
+  const getPromptsById = docSnapshot.data(); // Extracting data from the document snapshot
+
+  // Rendering the Form component with the data
   return <Form key={getPromptsById?.postId} {...getPromptsById} />;
 };
 
