04a344ae2152ce342862e818346bfaa9a42766cb
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-07-11 01:48:57 +0530

    Modified search page from client to serverside.

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,105 +1,74 @@
-// This page is currently under testing mode and will be available soon. 💀💀
+import Promptcard from "@/components/Promptcard";
+import { db } from "@/firebase/firebaseConfig";
+import { PostData } from "@/types/typescript.types";
+import { DocumentData, query, collection, orderBy, getDocs } from "firebase/firestore";
+import Fuse from "fuse.js";
+import { notFound } from "next/navigation";
 
-// "use client";
-// import Promptcard from "@/components/Promptcard";
-// import { db } from "@/firebase/firebaseConfig";
-// import { PostData } from "@/types/typescript.types";
-// import Loader from "@/components/Loader";
-// import { DocumentData, query, collection, orderBy, getDocs } from "firebase/firestore";
-// import {  useSearchParams } from "next/navigation";
-// import { useEffect, useState } from "react";
-// import Fuse from "fuse.js";
 type Props = {
-  params: { uid: string };
   searchParams: { [search_query: string]: string | string[] | undefined };
 };
-const page = ({ searchParams }: Props) => {
-  console.log("searchParams: " + searchParams?.search_query)
-  // const searchParams = useSearchParams();
-  // const search = searchParams.get("search_query");
-  // const cleanedSearchQuery = search?.toLowerCase().trim().replace(/[^\w\s]+/g, "").replace(/\s+/g, "");
-  // const [searchResult, setsearchResult] = useState<PostData[]>([]);
-  // const [loading, setLoading] = useState(false);
 
-  // const getSearchResult = async () => {
-  //   setLoading(true);
-  //   if(cleanedSearchQuery){ 
-  //     const snippetQuery = query(collection(db, "posts"), orderBy("tag"), orderBy("createdAt", "desc")); // Creating a query
-  //   const snippetDocs = await getDocs(snippetQuery); // Getting the query snapshot
-  //   // Perform fuzzy matching on the search query against the database results
-  //   const matchedResults = cleanedSearchQuery
-  //     ? fuzzyMatch(
-  //       cleanedSearchQuery,
-  //       snippetDocs.docs.map((doc) => doc.data())
-  //     )
-  //     : [];
-  //   const posts: PostData[] = matchedResults
-  //     .map((result) => {
-  //       const matchingDoc = snippetDocs.docs.find(
-  //         (doc) =>
-  //           doc.data().tag === result.tag ||
-  //           doc.data().prompt === result.prompt ||
-  //           doc.data().creatorName === result.creatorName
-  //       );
-  //       if (matchingDoc) {
-  //         const data = matchingDoc.data() as PostData;
-  //         return {
-  //           ...data,
-  //         };
-  //       }
-  //       return null;
-  //     })
-  //     .filter(Boolean) as PostData[];
-  //   setsearchResult(posts);
-  //   }
-  //   setLoading(false);
-  // };
+const Page = async ({ searchParams }: Props) => {
+  const search = searchParams?.search_query; // Extract the search query
 
-  // // Function to perform fuzzy matching
-  // function fuzzyMatch(query: string, results: DocumentData[]) {
-  //   const fuse = new Fuse(results, {
-  //     keys: ["tag", "prompt", "creatorName"], // Use 'tag' property for matching
-  //     includeScore: true,
-  //     threshold: 0.2, // Adjust the threshold value to control the matching sensitivity
-  //   });
+  let posts: PostData[] = []; // Declare an empty array to store the posts
 
-  //   const matches = fuse.search(query);
-  //   const sortedMatches = matches.sort((a, b) => a.score! - b.score!); // Sort based on the score
-  //   const matchedResults = sortedMatches.map((match) => match.item); // Extract the matched document
+  if (search) {
+    const snippetQuery = query(collection(db, "posts"), orderBy("tag"), orderBy("createdAt", "desc"));
+    const snippetDocs = await getDocs(snippetQuery);
 
-  //   return matchedResults;
-  // }
+    const matchedResults = search
+      ? fuzzyMatch(search, snippetDocs.docs.map((doc) => doc.data()))
+      : [];
 
-  // // Fetch the searched prompts
-  // useEffect(() => {
-  //   getSearchResult();
-  // }, [search]);
+    posts = matchedResults
+      .map((result: any) => {
+        const matchingDoc = snippetDocs.docs.find(
+          (doc) =>
+            doc.data().tag === result.tag ||
+            doc.data().prompt === result.prompt ||
+            doc.data().creatorName === result.creatorName
+        );
 
-  // // Render the Loader component while loading
-  // if (loading) {
-  //   return <div className="pt-2">
-  //     <Loader />;
-  //   </div>
-  // }
+        if (matchingDoc) {
+          const data = matchingDoc.data() as PostData;
+          return {
+            ...data,
+          };
+        }
+        return null;
+      })
+      .filter(Boolean) as PostData[];
+  }
+
+  // Function to perform fuzzy matching
+  function fuzzyMatch(query: any, results: DocumentData[]) {
+    const fuse = new Fuse(results, {
+      keys: ["tag", "prompt", "creatorName"],
+      includeScore: true,
+      threshold: 0.2,
+    });
+
+    const matches = fuse.search(query);
+    const sortedMatches = matches.sort((a, b) => a.score! - b.score!);
+    const matchedResults = sortedMatches.map((match) => match.item);
+
+    return matchedResults;
+  }
 
   return (
     <>
       <div className="max-w-5xl mx-auto px-2 xl:px-0 pt-6">
-        {/* <h1 className="md:text-5xl text-2xl text-center font-bold">
-          <span className="bg-gradient-to-r from-purple-500 via-pink-600 to-rose-500 bg-clip-text text-transparent text-center">
-            {searchResult.length === 0 ?"No Searched Results" : "Your Searched Results"}</span>
-        </h1> */}
-        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pt-8">
-          {(
-            searchResult.map((post) => (
-              <Promptcard key={post.postId} {...post} />
-            ))
-          )}
-        </div> */}
-        Hello
+        {posts.length === 0 && notFound()}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pt-8">
+          {posts.map((post) => (
+            <Promptcard key={post.postId} {...post} />
+          ))}
+        </div>
       </div>
     </>
   );
 };
 
-export default page;
+export default Page;
