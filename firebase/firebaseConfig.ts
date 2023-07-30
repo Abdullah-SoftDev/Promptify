8c63d1c869f1044c7e72c3dca7bdeb707f4809dd
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-07-30 16:21:57 +0530

    Modify firebase config

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -15,7 +15,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase for SSR
-const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
