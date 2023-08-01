fcf7c1b523cda229a2617b82641a72b5f60ec709
Abdullah-SoftDev <siddabdullahofficial@gmail.com>
2023-08-01 17:45:49 +0530

    Fix the metadata and severside page rendering.

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,13 @@
 import Feed from "@/components/Feed";
 import Footer from "@/components/Footer";
 import Herosection from "@/components/Herosection";
-import { Metadata } from "next";
 
 type Props = {
-  searchParams: { search_query?: string | string[] | undefined };
-};
-
-export async function generateMetadata({ searchParams }: Props): Promise<Metadata> {
-  const title = Array.isArray(searchParams.search_query)
-    ? searchParams.search_query[0]
-    : searchParams.search_query;
-
-  return {
-    title: `Search - ${title}` || '',
-  };
+  searchParams: { title: string };
 }
 
-const page = async () => {
+const page = async ({ searchParams }: Props) => {
+  console.log(searchParams.title)
   return (
     <>
       <Herosection />
--- app/trending/page.tsx
+++ app/trending/page.tsx
@@ -5,38 +5,29 @@ import { query, collection, orderBy, getDocs, limit, QueryDocumentSnapshot } fro
 import { Balancer } from "react-wrap-balancer";
 import { Metadata } from "next";
 
-// export const metadata: Metadata = {
-//   title: 'Trending Prompts | Promptify',
-//   description: 'Discover and create trending prompts for AI. Explore popular prompts created by the community and unleash your creativity. Copy and use prompts directly in AI tools!',
-// };
-
-type Props = {
-  searchParams: { search_query?: string | string[] | undefined };
+export const metadata: Metadata = {
+  title: 'Trending Prompts | Promptify',
+  description: 'Discover and create trending prompts for AI. Explore popular prompts created by the community and unleash your creativity. Copy and use prompts directly in AI tools!',
 };
 
-export async function generateMetadata({ searchParams }: Props): Promise<Metadata> {
-  const title = Array.isArray(searchParams.search_query)
-    ? searchParams.search_query[0]
-    : searchParams.search_query;
-
-  return {
-    title: `Search - ${title}` || '',
-  };
+type Props = {
+  searchParams: { title: string };
 }
 
 const Page = async ({ searchParams }: Props) => {
-  console.log(searchParams)
-  const postsRef = collection(db, "posts"); // Getting reference to "posts" collection
-  const snippetQuery = query(postsRef, orderBy("like", "desc"), limit(5)); // Creating a query to get top 5 posts ordered by likes
-  const snippetDocs = await getDocs(snippetQuery); // Getting the query snapshot
+  console.log(searchParams.title)
+  const postsRef = collection(db, "posts");
+  const snippetQuery = query(postsRef, orderBy("like", "desc"), limit(5));
+  const snippetDocs = await getDocs(snippetQuery);
   const fetchedPosts: PostData[] = snippetDocs.docs.map(
     (doc: QueryDocumentSnapshot) => {
-      const data = doc.data() as PostData; // Casting document data as PostData type
+      const data = doc.data() as PostData;
       return {
         ...data,
       };
     }
-  );
+  )
+
   return (
     <>
       <div className="max-w-5xl mx-auto px-2 xl:px-0 pt-10 sm:pt-14 pb-14">
@@ -47,7 +38,6 @@ const Page = async ({ searchParams }: Props) => {
         </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 pt-6 sm:pt-14">
           {fetchedPosts.map((post) => (
-            // Rendering Promptcard component for each fetched post
             <Promptcard key={post.postId} {...post} />
           ))}
         </div>
